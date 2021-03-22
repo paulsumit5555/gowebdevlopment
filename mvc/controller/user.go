4d9b9db27e42222b17dec3dbd4998a3a43b70a9d
paulsumit555 <paulsumit555@gmail.com>
2021-03-22 17:53:58 +0530

    added

--- mvc/controller/user.go
+++ mvc/controller/user.go
@@ -1,6 +1,9 @@
 package controller
 
 import (
+	"html/template"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
 	"fmt"
 )
 
@@ -8,7 +11,12 @@ type UserController struct {
 	 tpl *template.Template
 }
 
+func GetUserController(tpl *template.Template) *UserController{
+	return &UserController{tpl}
+}
 
-func (uc UserController) home(){
-	fmt.Println(tpl)
+
+func (uc UserController) Home(w http.ResponseWriter, _ *http.Request, _ httprouter.Params){
+	fmt.Println(uc.tpl)
+	uc.tpl.ExecuteTemplate(w,"index.gohtml",nil)
 }
--- mvc/main.go
+++ mvc/main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
-	//"github.com/julienschmidt/httprouter"
+	"github.com/julienschmidt/httprouter"
 	//"gopkg.in/mgo.v2"
 	"net/http"	
 	"github.com/paulsumit5555/gowebdevlopment/mvc/controller"
+	"html/template"
+	"fmt"		
 )
 
-func main(){
+var tpl *template.Template
 
-	http := httprouter.New()
-	//http.GET("/","index")
+func init(){
+	fmt.Println("inside init")
+tpl = template.Must(template.ParseGlob("view/*"))
+}
+
+func main(){
+	router := httprouter.New()
+	uc := controller.GetUserController(tpl)
+	fmt.Println(uc)
+	router.GET("/home",uc.Home)
+	http.ListenAndServe("localhost:8080", router)
 
 }
+
+/*func Home(w http.ResponseWriter, _ *http.Request, _ httprouter.Params){
+	fmt.Println(tpl)
+	tpl.ExecuteTemplate(w,"index.gohtml",nil)
+}*/
